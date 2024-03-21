0671f65a6d6c2e828ee3e18e9a9b15a8bda2d8bf
AmireuxLuna <amireux_jiao@163.com>
2024-03-21 23:50:24 +0800

    测试linux下的GitHub和git

---
+++ lisp/init-const.el
@@ -0,0 +1,19 @@
+;;; init-const.el --- This file defines constants -*- lexical-binding: t —*-
+;;; Commentart
+
+;;; Code
+
+
+(defconst *is-mac* (eq system-type 'darwin)
+  "判断是否是mac")
+(defconst *is-linux* (eq system-type 'gnu/linux)
+  "判断是否是Linux")
+(defconst *is-windows* (or (eq system-type 'ms-dos) (eq system-type 'windows-nt))
+  "判断是否是Windows")
+
+
+(provide 'init-const)
+;;; init-const.el ends here
+
+
+
