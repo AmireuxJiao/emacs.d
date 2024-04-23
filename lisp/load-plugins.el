3c298ebd722ce50e18f9009afb47652c55e2ee19
AmireuxLuna <amireux_jiao@163.com>
2024-04-23 09:38:13 +0800

    config c-mode yasnippet-snippet

--- lisp/load-plugins.el
+++ lisp/load-plugins.el
@@ -140,38 +140,22 @@
   :config (setq swiper-action-recenter t
 		swiper-include-line-number-in-search t))
 
-<<<<<<< HEAD
-=======
-(use-package yasnippet
-  :ensure t
-  :init (yas-global-mode 1)
-  :hook
-  (prog-mode . yas-minor-mode)
-  :config
-  (yas-reload-all)
-  
-  (define-key yas-minor-mode-map [(tab)]        nil)
-  (define-key yas-minor-mode-map (kbd "TAB")    nil)
-  (define-key yas-minor-mode-map (kbd "<tab>")  nil)
-  :bind
-  (("M-/" . 'company-yasnippet)))
 
-
-(use-package yasnippet-snippets
-  :ensure t
-  :after yasnippet)
-
->>>>>>> dd6af3b0f0d52f2d8ef82a3807756217b604edad
 (use-package company
   :ensure t
   :init (global-company-mode)
   :config
-  (setq company-minimum-prefix-length 1)
-  (setq company-tooltip-align-annotations t)
-  (setq company-idle-delay 0.0)
-  (setq company-show-numbers t)
-  (setq company-selection-wrap-around t)
-  (setq company-transformers '(company-sort-by-occurrence)))
+  (setq company-minimum-prefix-length 1
+	company-tooltip-align-annotations t
+	company-tooltip-idle-delay 0.0
+	company-show-numbers t
+	company-selection-wrap-around t
+	company-transformers '(company-sort-by-occurrence)))
+
+(use-package company-box
+  :ensure t
+  :if window-system
+  :hook (company-mode . company-box-mode))
 
 (use-package yasnippet
   :ensure t
@@ -180,11 +164,11 @@
   (prog-mode . yas-minor-mode)
   :config
   (yas-reload-all)
-  
   (define-key yas-minor-mode-map [(tab)]        nil)
   (define-key yas-minor-mode-map (kbd "TAB")    nil)
   (define-key yas-minor-mode-map (kbd "<tab>")  nil)
-  (defvar company-mode/enable-yas t "Enable yasnippet for all backends.")
+  (defvar company-mode/enable-yas t
+    "Enable yasnippet for all backends.")
   (defun company-mode/backend-with-yas (backend)
     (if (or (not company-mode/enable-yas) (and (listp backend)
 					       (member 'company-yasnippet backend)))
@@ -195,33 +179,6 @@
 (use-package yasnippet-snippets
   :ensure t
   :after yasnippet)
-
-;; (use-package company
-;;   :ensure t
-;;   :init (global-company-mode)
-;;   :config
-;;   (setq company-minimum-prefix-length 1)
-;;   (setq company-tooltip-align-annotations t)
-;;   (setq company-idle-delay 0.0)
-;;   (setq company-show-numbers t)
-;;   (setq company-selection-wrap-around t)
-;;   (setq company-transformers '(company-sort-by-occurrence)))
-;;   (defvar company-mode/enable-yas t "Enable yasnippet for all backends.")
-;;   (defun company-mode/backend-with-yas (backend)
-;;     (if (or (not company-mode/enable-yas) (and (listp backend)
-;; 					       (member 'company-yasnippet backend)))
-;; 	backend (append (if (consp backend) backend (list backend))
-;; 			'(:with company-yasnippet))))
-;;   (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
-
-
-(use-package company-box
-  :ensure t
-  :if window-system
-  :hook (company-mode . company-box-mode))
-
-
-
 
 (use-package which-key
   :ensure t
